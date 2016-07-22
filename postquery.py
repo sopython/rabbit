5d23d40137d4f19889a55085b14f43edc35b158c
Kevin <kevin.m.smiley@gmail.com>
2016-07-22 09:05:18 -0400

    Properly encode text parameter

--- postquery.py
+++ postquery.py
@@ -1,10 +1,10 @@
 import requests
 import json
-import html
 import sys
 import websockets
 import asyncio
 import config
+from urllib.parse import quote
 from bs4 import BeautifulSoup as BS
 
 
@@ -69,7 +69,7 @@ def query_messages_test(roomid):
             print("Whoops, not a message")
 
 def post_message_test(roomid, text):
-    s = "text={}&fkey={}".format(html.escape(text), fkey)
+    s = "text={}&fkey={}".format(quote(text), fkey)
 
     x = requests.post(
         "https://chat.stackoverflow.com/chats/{}/messages/new".format(roomid),
